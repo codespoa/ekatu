99e30d744342a5152252ef212b0757dbfd24cd77
Cadu Alves <carlos.eduardo@fatordigital.com.br>
2020-09-02 13:12:08 -0300

    Ajust tooltip

--- src/components/Reduza/index.tsx
+++ src/components/Reduza/index.tsx
@@ -9,7 +9,10 @@ type DTO = {
 }
 
 const Reduza: React.FC<DTO> = () => {
-  const [showContent, setShowContent] = useState(false)
+  const [showContent, setShowContent] = useState('l1')
+  const [showContentText, setShowContentText] = useState(
+    'Reduza gastos e o impacto da sua empresa no mundo'
+  )
 
   return (
     <ContainerReduza>
@@ -18,46 +21,76 @@ const Reduza: React.FC<DTO> = () => {
           <div className="select">
             <ul>
               <a>
-                <li>
-                  <p className="green"></p>
+                <li
+                  onClick={() => {
+                    setShowContent('l1')
+                    setShowContentText(
+                      'Reduza gastos e o impacto da sua empresa no mundo'
+                    )
+                  }}
+                >
+                  <p className={showContent === 'l1' ? 'green' : ''}></p>
                   <div className="box-dialog"></div>
                 </li>
               </a>
               <a href="#">
-                <li>
-                  <p></p>
+                <li
+                  onClick={() => {
+                    setShowContent('l2')
+                    setShowContentText('teste 2')
+                  }}
+                >
+                  <p className={showContent === 'l2' ? 'green' : ''}></p>
                   <div className="box-dialog"></div>
                 </li>
               </a>
               <a href="#">
-                <li>
-                  <p></p>
+                <li
+                  onClick={() => {
+                    setShowContent('l3')
+                    setShowContentText('teste 3')
+                  }}
+                >
+                  <p className={showContent === 'l3' ? 'green' : ''}></p>
                   <div className="box-dialog"></div>
                 </li>
               </a>
               <a href="#">
-                <li>
-                  <p></p>
+                <li
+                  onClick={() => {
+                    setShowContent('l4')
+                    setShowContentText('teste 4')
+                  }}
+                >
+                  <p className={showContent === 'l4' ? 'green' : ''}></p>
                   <div className="box-dialog"></div>
                 </li>
               </a>
               <a href="#">
-                <li>
-                  <p></p>
+                <li
+                  onClick={() => {
+                    setShowContent('l5')
+                    setShowContentText('teste 5')
+                  }}
+                >
+                  <p className={showContent === 'l5' ? 'green' : ''}></p>
                   <div className="box-dialog"></div>
                 </li>
               </a>
               <a href="#">
-                <li>
-                  <p></p>
+                <li
+                  onClick={() => {
+                    setShowContent('l6')
+                    setShowContentText('teste 6')
+                  }}
+                >
+                  <p className={showContent === 'l6' ? 'green' : ''}></p>
                   <div className="box-dialog"></div>
                 </li>
               </a>
             </ul>
             <div className="text">
-              <h1 className="textPrimary">
-                Reduza gastos e o impacto da sua empresa no mundo
-              </h1>
+              <h1 className="textPrimary">{showContentText}</h1>
             </div>
           </div>
         </div>
